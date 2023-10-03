d4c92e10a7266a02ec244d2bf9b7291a83351efb
matheustkaczyk <matheus111904@hotmail.com>
2023-10-03 11:17:01 -0300

    refact: using newly created response object

--- api/handler.js
+++ api/handler.js
@@ -3,6 +3,7 @@
 const { pbkdf2Sync } = require('crypto');
 const { MongoClient, ObjectId } = require('mongodb');
 const { sign, verify } = require('jsonwebtoken');
+const { buildResponse } = require('./utils');
 
 let connectionPool = null;
 
@@ -31,40 +32,21 @@ function extractBody(event) {
   };
 }
 
-
 module.exports.authorize = async (event) => {
   const [type, token] = event.headers.Authorization.split(' ');
 
   if (type !== 'Bearer') {
-    return {
-      statusCode: 401,
-      body: JSON.stringify({ error: 'Invalid token type' }),
-      headers: {
-        'Content-Type': 'application/json',
-      }
-    }
+    return buildResponse(401, { error: 'Invalid token type' })
   }
 
   if(!token) {
-    return {
-      statusCode: 401,
-      body: JSON.stringify({ error: 'Token not provided' }),
-      headers: {
-        'Content-Type': 'application/json',
-      }
-    }
+    return buildResponse(401, { error: 'Token not provided' })
   }
 
   const decodedToken = verify(token, process.env.JWT_SECRET);
 
   if (!decodedToken) {
-    return {
-      statusCode: 401,
-      body: JSON.stringify({ error: 'Invalid token' }),
-      headers: {
-        'Content-Type': 'application/json',
-      }
-    }
+    return buildResponse(401, { error: 'Invalid token' })
   }
 
   return decodedToken;
@@ -79,39 +61,19 @@ module.exports.login = async (event) => {
   const user = await collection.findOne({ username, password: hashedPass });
 
   if (!user) {
-    return {
-      statusCode: 401,
-      body: JSON.stringify({ error: 'Invalid credentials' }),
-      headers: {
-        'Content-Type': 'application/json',
-      }
-    }
+    return buildResponse(401, { error: 'Invalid credentials' })
   }
 
   const token = sign({ id: user._id, username }, process.env.JWT_SECRET, { expiresIn: '1h' });
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      token
-    }),
-    headers: {
-      'Content-Type': 'application/json',
-    }
-  }
+  return buildResponse(200, { token });
 }
 
 module.exports.createUser = async (event) => {
   const { username, password } = extractBody(event);
 
   if (!username || !password) {
-    return {
-      statusCode: 422,
-      body: JSON.stringify({ error: 'Invalid request' }),
-      headers: {
-        'Content-Type': 'application/json',
-      }
-    }
+    return buildResponse(422, { error: 'Invalid request' })
   }
 
   const hashedPass = pbkdf2Sync(password, process.env.SALT, 100000, 64, 'sha512').toString('hex');
@@ -120,16 +82,7 @@ module.exports.createUser = async (event) => {
   const collection = await client.collection('users');
   const { insertedId } = await collection.insertOne({ username, password: hashedPass });
 
-  return {
-    statusCode: 201,
-    body: JSON.stringify({
-      id: insertedId,
-      username
-    }),
-    headers: {
-      'Content-Type': 'application/json',
-    }
-  }
+  return buildResponse(201, { id: insertedId, username });
 }
 
 module.exports.sendResponse = async (event) => {
@@ -157,19 +110,7 @@ module.exports.sendResponse = async (event) => {
   const collection = await client.collection('results');
   const { insertedId } = await collection.insertOne(result);
 
-  return {
-    statusCode: 201,
-    body: JSON.stringify({
-      resultId: insertedId,
-    __hypermedia: {
-      href: `/results.html`,
-      query: { id: insertedId }
-    }
-  }),
-  headers: {
-    'Content-Type': 'application/json',
-  }
-  }
+  return buildResponse(201, { resultId: insertedId, username, __hypermedia: { href: `/results.html`, query: { id: insertedId } } });
 }
 
 module.exports.getResult = async (event) => {
@@ -181,20 +122,8 @@ module.exports.getResult = async (event) => {
   const result = await collection.findOne({ _id: new ObjectId(event.pathParameters.id) });
 
   if (!result) {
-    return {
-      statusCode: 404,
-      body: JSON.stringify({ error: 'Result not found' }),
-      headers: {
-        'Content-Type': 'application/json',
-      }
-    }
+    return buildResponse(404, { error: 'Result not found' })
   }
-  
-  return {
-    statusCode: 200,
-    body: JSON.stringify(result),
-    headers: {
-      'Content-Type': 'application/json',
-    }
-  }
+
+  return buildResponse(200, result);
 }
