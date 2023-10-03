641f2576c375e2d0f7937bdfe2206ed66d777bb3
matheustkaczyk <matheus111904@hotmail.com>
2023-10-03 11:44:54 -0300

    refact: using newly created decoupled functions

--- api/handler.js
+++ api/handler.js
@@ -1,23 +1,8 @@
 'use strict';
 
-const { pbkdf2Sync } = require('crypto');
-const { MongoClient, ObjectId } = require('mongodb');
 const { sign, verify } = require('jsonwebtoken');
 const { buildResponse } = require('./utils');
-
-let connectionPool = null;
-
-async function connectDatabase() {
-  if (connectionPool)  return connectionPool;
-  
-  const client = new MongoClient(process.env.MONGODB_CONNECTION_STRING);
-
-  const connection = await client.connect();
-
-  connectionPool = connection.db(process.env.MONGODB_DB_NAME);
-
-  return connectionPool;
-}
+const { getUserByCredentials, createUser, createResult, getResult } = require('./database');
 
 function extractBody(event) {
   const body = event?.body;
@@ -54,11 +39,8 @@ module.exports.authorize = async (event) => {
 
 module.exports.login = async (event) => {
   const { username, password } = extractBody(event);
-  const hashedPass = pbkdf2Sync(password, process.env.SALT, 100000, 64, 'sha512').toString('hex');
-
-  const client = await connectDatabase();
-  const collection = await client.collection('users');
-  const user = await collection.findOne({ username, password: hashedPass });
+  
+  const user = await getUserByCredentials(username, password);
 
   if (!user) {
     return buildResponse(401, { error: 'Invalid credentials' })
@@ -76,11 +58,7 @@ module.exports.createUser = async (event) => {
     return buildResponse(422, { error: 'Invalid request' })
   }
 
-  const hashedPass = pbkdf2Sync(password, process.env.SALT, 100000, 64, 'sha512').toString('hex');
-
-  const client = await connectDatabase();
-  const collection = await client.collection('users');
-  const { insertedId } = await collection.insertOne({ username, password: hashedPass });
+  const { insertedId } = await createUser(username, password);
 
   return buildResponse(201, { id: insertedId, username });
 }
@@ -106,9 +84,7 @@ module.exports.sendResponse = async (event) => {
     totalAnswers: answers.length
   }
 
-  const client = await connectDatabase();
-  const collection = await client.collection('results');
-  const { insertedId } = await collection.insertOne(result);
+  const { insertedId } = await createResult(result);
 
   return buildResponse(201, { resultId: insertedId, username, __hypermedia: { href: `/results.html`, query: { id: insertedId } } });
 }
@@ -117,9 +93,7 @@ module.exports.getResult = async (event) => {
   const authResult = await this.authorize(event);
   if (authResult.statusCode === 401) return authResult;
 
-  const client = await connectDatabase();
-  const collection = await client.collection('results');
-  const result = await collection.findOne({ _id: new ObjectId(event.pathParameters.id) });
+  const result = await getResult(event.queryStringParameters.id);
 
   if (!result) {
     return buildResponse(404, { error: 'Result not found' })
